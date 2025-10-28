9f90df8a78f9624c9ea216e909c77a65a28c3e27
Promise Duke <promiseduke@gmail.com>
2025-10-28 23:42:37 +0100

    adding serverless and adjustung netlify functions

--- twig-app/netlify/functions/server.js
+++ twig-app/netlify/functions/server.js
@@ -2,22 +2,22 @@ import serverless from 'serverless-http';
 import express from 'express';
 import twig from 'twig';
 import path from 'path';
-import { fileURLToPath } from 'url';
 
 const app = express();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.set('views', path.join(__dirname, '../../views'));
+
+// Use process.cwd() to resolve paths safely in Netlify
+app.set('views', path.join(process.cwd(), 'views'));
 app.set('view engine', 'twig');
 app.engine('twig', twig.__express);
-app.use(express.static(path.join(__dirname, '../../public')));
+app.use(express.static(path.join(process.cwd(), 'public')));
 
 app.get('/', (req, res) => {
   res.render('pages/home', { pageTitle: 'Home - Ticketly (Twig)' });
 });
+
 // Add other routes here...
 
 export const handler = serverless(app);
